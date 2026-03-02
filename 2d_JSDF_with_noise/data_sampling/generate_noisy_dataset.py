43508aa661311945c72b4e269d57c944ab5c230d
parallels <albusfang962@gmail.com>
2026-03-02 15:01:07 -0500

    sampling in normalized space

--- 2d_JSDF_with_noise/data_sampling/generate_noisy_dataset.py
+++ 2d_JSDF_with_noise/data_sampling/generate_noisy_dataset.py
@@ -5,12 +5,12 @@ from turtle_2d_sampler_noisy_data import TurtleBot2DGeometryDataset
 
 sampler = TurtleBot2DGeometryDataset(
     dataset_path="../dataset",
-    radius=0.105,
-    xlim=(-2, 2),
-    ylim=(-2, 2)
+    radius=0.0105,
+    xlim=(-0.5, 0.5),
+    ylim=(-0.36, 0.36)
 )
 
 sampler.generate_batch(
-    batch_size=3000,          # number of robot poses
+    batch_size=600,          # number of robot poses
     points_per_pose=1500
 )
